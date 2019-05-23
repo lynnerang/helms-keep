73f6799bc8e8e0cb219cd2ec64f93466f73c69fa
Lynne Rang <lynne.rang@gmail.com>
2019-05-23 10:39:54 -0600

    Add routing for home and new-note.

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import QuestContainer from '../QuestContainer/QuestContainer';
 import QuestForm from '../QuestForm/QuestForm';
+import { NavLink } from 'react-router-dom';
 
 export class Main extends Component {
-  state = {
-    showForm: false
-  }
-  
-  closeForm = () => {
-    this.setState({ showForm: false })
-  }
+	state = {
+		showForm: false
+	};
 
-  render() {
-    const form = this.state.showForm ? <QuestForm closeForm={this.closeForm} /> : null;
+	render() {
+		const form = this.state.showForm ? <QuestForm closeForm={this.closeForm} /> : null;
 
-    return (
-      <main className="Main">
-        {form}
-        <div className="main-header">
-          <h1 className="page-title">My Quests</h1>
-          <div className="controls">
-            <div className="search-bar">
-              <input className="search-input" placeholder="Search your quests..."/>
-              <i className="fas fa-search" />
-            </div>
-            <select className="quest-filter"></select>
-          </div>
-        </div>
-        <QuestContainer />
-        <button className="add-quest-btn" type="button"><i className="fas fa-plus" onClick={() => this.setState({showForm: true})}></i></button>
-      </main>
-    )
-  }
+		return (
+			<main className="Main">
+				{form}
+				<div className="main-header">
+					<h1 className="page-title">My Quests</h1>
+					<div className="controls">
+						<div className="search-bar">
+							<input className="search-input" placeholder="Search your quests..." />
+							<i className="fas fa-search" />
+						</div>
+						<select className="quest-filter" />
+					</div>
+				</div>
+				<QuestContainer />
+				<NavLink to="/new-note" className="add-quest-btn" type="button">
+					<i className="fas fa-plus" />
+				</NavLink>
+			</main>
+		);
+	}
 }
 
-export default Main
+export default Main;

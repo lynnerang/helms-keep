6ed794ff811f05b6798bea93d1a2453ddc1eb738
Max Silver <Maxbsilver@gmail.com>
2019-05-23 15:01:51 -0600

    add styling to progress bars

--- client/src/containers/ExperienceBar/ExperienceBar.jsx
+++ client/src/containers/ExperienceBar/ExperienceBar.jsx
@@ -1,27 +1,34 @@
 import { Line } from "rc-progress";
 
 import React, { Component } from "react";
+import Star from "../../assets/Star.png";
+import Life from "../../assets/Life.png";
 
 export class ProgressBarIndicator extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      percent: 10
-    };
-  }
-  handleClick = () => {
-      this.setState({percent: this.state.percent+10})
-  }
   render() {
     return (
-      <div style={{ margin: 10, width: 200 }}>
-        <Line
-          className="progress-bar"
-          strokeWidth="4"
-          percent={this.state.percent}
-        />
-        <button onClick={this.handleClick}>CLICK ME!</button>
+      <div className="bar-container" style={{ margin: 10, width: 200 }}>
+        <div>
+          <img src={Star} style={{ width: 40 }} />
+          <Line
+            className="progress-bar"
+            strokeColor="#FEEC26"
+            strokeWidth="4"
+            percent={50}
+          />
+          <span className="progress-text">50/100</span>
+        </div>
+        <div>
+          <img src={Life} style={{ width: 40 }} />
+
+          <Line
+            className="progress-bar"
+            strokeColor="#FF77A8"
+            strokeWidth="4"
+            percent={25}
+          />
+          <span className="progress-text">25/100</span>
+        </div>
       </div>
     );
   }

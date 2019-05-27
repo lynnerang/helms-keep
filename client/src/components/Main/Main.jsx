d7197a63bea2544bc151cbc0d1320b5599a3339e
Lynne Rang <lynne.rang@gmail.com>
2019-05-27 12:12:26 -0600

    Add tests for Main component, event listeners need help.

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -7,13 +7,10 @@ import { searchQuests } from '../../helper';
 import { addQuest } from '../../actions';
 
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-      results: null,
-    };
+export class Main extends Component {
+  state = {
+    query: '',
+    results: null,
   }
 
   handleChange = e => {
